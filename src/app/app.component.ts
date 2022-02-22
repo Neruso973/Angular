14804bd175e6ab5ea5692ec2cf11336c9703b538
Neruso973 <nelson.ntepp@icloud.com>
2022-02-22 22:41:16 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'First';
+  title = "Bienvenue sur le site de Nelson !"
 }
